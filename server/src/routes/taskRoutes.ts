4377f0da539db86b6dcfd04345d6e1f229fdc9a0
Chizycodes <okwudirejoy@gmail.com>
2025-06-24 00:24:53 +0100

    tasks api

---
+++ server/src/routes/taskRoutes.ts
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import { createTasks, getTasks } from "../controllers/taskController";
+
+const router = Router();
+
+router.get("/", getTasks);
+router.post("/", createTasks);
+export default router;
