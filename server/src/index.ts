110a43fb1c8c80d41c429047f2b2941ad0fd3e68
Chizycodes <okwudirejoy@gmail.com>
2025-06-26 12:07:27 +0100

    team API

--- server/src/index.ts
+++ server/src/index.ts
@@ -8,11 +8,13 @@ import morgan from "morgan";
 import projectRoutes from "./routes/projectRoutes";
 import taskRoutes from "./routes/taskRoutes";
 import searchRoutes from "./routes/searchRoutes";
+import userRoutes from "./routes/userRoutes";
+import teamRoutes from "./routes/teamRoutes";
 
 /* CONFIGURATIONS */
 dotenv.config();
 const app = express();
-app.use(express.json())
+app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
@@ -21,16 +23,18 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
 /* ROUTES */
-app.get("/", (req, res)=> {
-  res.status(200).json({ message: "This is home route" });
-})
+app.get("/", (req, res) => {
+	res.status(200).json({ message: "This is home route" });
+});
 
-app.use("/projects", projectRoutes)
+app.use("/projects", projectRoutes);
 app.use("/tasks", taskRoutes);
-app.use("/search", searchRoutes)
+app.use("/search", searchRoutes);
+app.use("/users", userRoutes);
+app.use("/teams", teamRoutes);
 
 /* SERVER */
 const PORT = process.env.PORT || 8001;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-})
+	console.log(`Server is running on port ${PORT}`);
+});
